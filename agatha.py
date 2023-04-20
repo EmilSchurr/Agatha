adeb96d969a31bfcbe5810528b0108bab49786a9
hyperabstrakt <36825522+EmilSchurr@users.noreply.github.com>
2023-04-20 16:32:24 +0200

    Update agatha.py

--- agatha.py
+++ agatha.py
@@ -1,114 +1,76 @@
 import discord
-import asyncio
-import time
-#import youtube_dl
-import functools
-import itertools
-import math
-import random
 from discord.ext import commands
-from async_timeout import timeout
+from discord.utils import get
+from discord import FFmpegPCMAudio
 import os
-from os import listdir
-from os.path import isfile, join
-import mysql.connector
+import asyncio
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+import random
 
-print('Agetha is under the water and needs your help, so come by her office to discuss the details')
-
-command_prefix = "!"
-client = discord.Client(command_prefix = '!')
-@client.event
+bot = commands.Bot(command_prefix='!', description='Agatha', intents=discord.Intents.all())
+@bot.event
 async def on_ready(): 
     print('Agatha is at your service')
-    #print('Agetha is under the water and needs your help, so come by her office to discuss the details')
-    # nötig?
-    # jaaa??!
-@client.event
-async def on_message(message):
-    #Check if the author is the bot
-    if message.author.bot:
-        return
-    #split the message and store in agrment array  
-    argument = message.content[1:].lower().split()
-    if message.content.startswith(command_prefix):       
-        #here should follow a help command
-        #now there is a hlep command
-        if argument[0] == "help":
-            await message.channel.send("Management Commands: clear [number of messages], leave, ping, load, top [number of rows]")
-            await message.channel.send("https://emilano.de/assets/commands.png")
-        #ping    
-        if argument[0] == "ping":
-            await message.channel.send('My ping is ' + str(round(client.latency * 1000)) + ' ms, how about yours?')
-        #disconnect
-        if argument[0] == "leave":
-            await voicechannel.disconnect()
-        #purge
-        if argument[0] == "clear":
-            amount = int(argument[1])+1
-            deleted = await message.channel.purge(limit=amount)
-            await message.channel.send('Deleted {} message(s)'.format(len(deleted)-1))
-            await asyncio.sleep(5)
-            await message.channel.purge(limit=1)
-        #normalize
-        if argument[0] == "load":
-            await message.channel.send('Copying files...')
-            await message.channel.send('Applying earrape protection...')
-            for filename in os.listdir('/discord_bots/agatha/mp3/'):
-                os.system('ffmpeg-normalize /discord_bots/agatha/mp3/'+filename+' -o normalized/'+filename+' -c:a mp3')
-            await message.channel.purge(limit=2)
-            await message.channel.send('Done')
-        #restart
-        if argument[0] == "restart":
-            await message.channel.send('Restarting... This may take a while')
-            os.system('/discord_bots/agatha/agatha.sh')
-            exit()
-        #scoreboard in discord
-        if argument[0] == "top":
-            cnx = mysql.connector.connect(host="*****", user="*****", password="*****", database="*****")
-            cursor = cnx.cursor()
-            sql = str('SELECT `command`, `counter` FROM `counter` ORDER BY `counter` DESC LIMIT '+ argument[1])
-            cursor.execute(sql)
-            for row in cursor:
-                await message.channel.send(row)
-            cursor.close()
-            cnx.close()
-        #play filename    
+@bot.command()
+async def play(ctx, command_name: str):
+    file_path = f"files/dir{command_name}.mp3"
+    if os.path.isfile(file_path):
+        voice_channel = ctx.author.voice.channel
+        voice_client = await voice_channel.connect()
+        source = FFmpegPCMAudio(file_path)
+        voice_client.play(source)
+        while voice_client.is_playing():
+            await asyncio.sleep(1)
+        await voice_client.disconnect()
+    else:
+        await ctx.send(f"Der Oger wurde Betrogen und konnte {command_name} nicht finden")
+@bot.command()
+async def load(ctx):
+    await ctx.send("Scanning for new files...")
+    await ctx.send("Deepfrying audio files...")
+    for filename in os.listdir('upload/dir'):
+                os.system('ffmpeg-normalize upload/dir'+filename+' -o files/dir'+filename+' -c:a mp3')
+                os.system('rm -f upload/dir'+filename)
+    await ctx.channel.purge(limit=2)
+    await ctx.send('Done')
+@bot.command()
+async def commands(ctx):
+    logo = Image.open('/var/www/html/assets/hyper_logo.png')
+    image_width = 1280
+    image_height = 800
+    font_size = 24
+    image = Image.new(mode='RGBA', size=(1300, 820), color=(0, 0, 0, 0))
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype('/var/www/html/assets/Rubik-Regular.ttf', font_size)
+    number = str(random.randint(1000000000, 9999999999))#create a random number so discord renders the image with new commands
+    text = number
+    filenames = os.listdir('/home/emil_schurr/normalized')
+    commands_str = ", ".join([os.path.splitext(filename)[0] for filename in filenames])
+    lines = []
+    current_line = ""
+    for name in commands_str.split(", "):
+        new_line = current_line + ", " + name if current_line else name
+        text_width, _ = font.getsize(new_line)
+        if text_width > image_width:
+            lines.append(current_line)
+            current_line = name
         else:
-            channel = message.author.voice.channel
-            voicechannel = await channel.connect()
-            voicechannel.play(discord.FFmpegPCMAudio(normalized/' + argument[0] + '.mp3',)) 
-            while voicechannel.is_playing():
-                if argument[0] == "leave":
-                    await voicechannel.disconnect()
-                else:
-                    time.sleep(2)
-            else:
-                await voicechannel.disconnect()
-            #for scoreboard
-            cnx = mysql.connector.connect(host="*****", user="*****", password="*****", database="*****")
-            cursor = cnx.cursor()
-            arg = str("'" + argument[0] + "'")
-            sql = str("SELECT `counter` FROM `counter` WHERE `command`= " +arg)
-            val = (arg)
-            cursor.execute(sql)
-            c = cursor.fetchone()
-            print(c)
-            if c == None:
-                with cnx.cursor() as cursor:
-                    sql = "INSERT INTO counter (command, counter, uploaded, nr) VALUES (%s, %s, %s, %s)"
-                    val = (argument[0], "1", "hyperabstrakt", "1")
-                    cursor.execute(sql, val)
-                    cnx.commit()
-                cursor.close()
-            else:
-                with cnx.cursor() as cursor:
-                    cn = int(str(c)[1:-2])
-                    sql = str("UPDATE `counter` SET `counter`=" + str(cn+1) +  " WHERE `command`= " +arg)
-                    val = (cn+1, arg)
-                    print(cn+1)
-                    print(sql)
-                    cursor.execute(sql)
-                    cnx.commit()
-                cursor.close()
-client.run('*****')  
+            current_line = new_line
+    if current_line:
+        lines.append(current_line)
+    line_height = font_size + 10
+    x = 10
+    y = 10
+    for line in lines:
+        draw.text((x, y), line, font=font, fill=(255, 255, 255))
+        y += line_height
+    #hyper-important hyper-logo
+    logo_size = (int(image.width * 0.1), int(image.height * 0.11))
+    logo_x = image.width - logo_size[0]
+    logo_y = image.height - logo_size[1]
+    logo = logo.resize(logo_size)
+    image.paste(logo, (logo_x, logo_y))
+    image.save('/var/www/html/bot/commands/'+number+'.png')
 
+    await ctx.send("https://emilano.de/bot/commands/"+number+".png")
+bot.run('SECRET')
